633f14eab3dad429856ec75e9a3a7b443d591de8
MatteoSpinelli <matteospinelli10@gmail.com>
2024-02-17 21:09:28 +0100

    feat: add a second ball with cosine armonic motion

--- osciallation/horizontal_pendulum/sketch.js
+++ osciallation/horizontal_pendulum/sketch.js
@@ -1,5 +1,5 @@
-const amplitude = 100
-const period = 120
+const amplitude = 250
+const period = 240
 
 
 function setup() {
@@ -10,8 +10,11 @@ function draw() {
   background(220);
   translate(width / 2, height / 2)
   let x = amplitude * sin(TWO_PI * frameCount / period)
+  let x1 = amplitude * cos(TWO_PI * frameCount / period)
   stroke(0)
   fill(127)
   circle(x, 0, 20)
+  circle(x1, 0, 20)
   line(0, 0, x, 0)
+  line(x1, 0, x, 0)
 }
